5b90948a8dba4d44d86d0dd173f57ca8fbcb588c
DESKTOP-9T2MG7U\User <ksjmgrkks@naver.com>
2021-12-15 02:16:25 +0900

    Comment: CommunityViewModel 주석 추가 2021/12/15 LiveData, MutableLiveData 영어 주석 붙여넣기. 추후에 각각에 대한 해석과 둘의 차이에 대한 주석을 기록해야함.

--- app/src/main/java/kks/jpeople/ui/community/CommunityViewModel.kt
+++ app/src/main/java/kks/jpeople/ui/community/CommunityViewModel.kt
@@ -87,9 +87,24 @@ class CommunityViewModel : ViewModel() {
     println("$person")
     //Person(name=James, age=56)*/
 
-    //LiveData 란?
-    //
+    /**
+     * LiveData 란?
+     *LiveData is a data holder class that can be observed within a given lifecycle. This means that an Observer can be added in a pair with a LifecycleOwner, and this observer will be notified about modifications of the wrapped data only if the paired LifecycleOwner is in active state. LifecycleOwner is considered as active, if its state is Lifecycle.State.STARTED or Lifecycle.State.RESUMED. An observer added via observeForever(Observer) is considered as always active and thus will be always notified about modifications. For those observers, you should manually call removeObserver(Observer).
+    An observer added with a Lifecycle will be automatically removed if the corresponding Lifecycle moves to Lifecycle.State.DESTROYED state. This is especially useful for activities and fragments where they can safely observe LiveData and not worry about leaks: they will be instantly unsubscribed when they are destroyed.
+    In addition, LiveData has onActive() and onInactive() methods to get notified when number of active Observers change between 0 and 1. This allows LiveData to release any heavy resources when it does not have any Observers that are actively observing.
+    This class is designed to hold individual data fields of ViewModel, but can also be used for sharing data between different modules in your application in a decoupled fashion.
+     */
+
+    /**
+     * MutableLiveData 란?
+     * LiveData which publicly exposes setValue(T) and postValue(T) method.
+    Type parameters:
+    <T> – The type of data hold by this instance
+     */
+
     //MutableLiveData 란? LiveData 와의 차이?
+    //
+
     private val _text = MutableLiveData<String>().apply {
         value = "순모임 프래그먼트입니다."
     }
