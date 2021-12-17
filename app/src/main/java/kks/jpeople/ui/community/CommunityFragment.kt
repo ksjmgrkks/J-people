4eb48a3f3e33c5367d36e534287839f0bbb15f59
kyuseong <ksjmgrkks@naver.com>
2021-12-17 10:08:38 +0900

    Comment: CommunityFragment 주석 추가 2021/12/17 kotlin it 문법에 대한 내용 정리를 추가하였고, viewLifecycleOwner에 대한 주석을 그대로 가져왔다. 추후 해석해서 커밋할 예정임.

--- app/src/main/java/kks/jpeople/ui/community/CommunityFragment.kt
+++ app/src/main/java/kks/jpeople/ui/community/CommunityFragment.kt
@@ -24,7 +24,6 @@ class CommunityFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //공부해야할 리스트 정리 :  observe, it, viewLifecycleOwner
 /**      ViewModelProvider 란?
          Activity 나 Fragment 에 ViewModel 을 제공하는 유틸리티 클래스입니다.
          Activity 나 Fragment 에 대한 기본 ViewModelProvider 를 생성자에게 전달하여 얻을 수 있습니다.*/
@@ -34,21 +33,40 @@ class CommunityFragment : Fragment() {
          코틀린 ::class.java 와 리플렉션(Reflection)
          링크 : https://yoon-dailylife.tistory.com/46*/
 
-        //kotlin it
-        //
-
-        //viewLifecycleOwner
-
-        //binding.root -> fragment_community.xml 을 의미함
+        /**
+         * viewLifecycleOwner
+         * Get a LifecycleOwner that represents the Fragment's View lifecycle. In most cases, this mirrors the lifecycle of the Fragment itself, but in cases of detached Fragments, the lifecycle of the Fragment can be considerably longer than the lifecycle of the View itself.
+        Namely, the lifecycle of the Fragment's View is:
+        created after onViewStateRestored(Bundle)
+        started after onStart()
+        resumed after onResume()
+        paused before onPause()
+        stopped before onStop()
+        destroyed before onDestroyView()
+        The first method where it is safe to access the view lifecycle is onCreateView(LayoutInflater, ViewGroup, Bundle) under the condition that you must return a non-null view (an IllegalStateException will be thrown if you access the view lifecycle but don't return a non-null view).
+        The view lifecycle remains valid through the call to onDestroyView(), after which getView() will return null, the view lifecycle will be destroyed, and this method will throw an IllegalStateException. Consider using getViewLifecycleOwnerLiveData() or FragmentTransaction.runOnCommit(Runnable) to receive a callback for when the Fragment's view lifecycle is available.
+        This should only be called on the main thread.
+        Overriding this method is no longer supported and this method will be made final in a future version of Fragment.
+        Returns:
+        A LifecycleOwner that represents the Fragment's View lifecycle.
+        Throws:
+        IllegalStateException – if the Fragment's View is null.
+         */
         communityViewModel =
             ViewModelProvider(this)[CommunityViewModel::class.java]
 
         _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+
+        /** binding.root -> fragment_community.xml 을 의미함 */
         val root: View = binding.root
 
         val textView: TextView = binding.textCommunity
         communityViewModel.text.observe(viewLifecycleOwner, Observer {
-
+            /** kotlin it에 대하여
+             * 출처 : https://tourspace.tistory.com/110
+             * 인자가 하나일 때 람다식 내부에서 it 으로 치환이 가능함.
+             * 즉, textString : String -> textView.text = textString 과 textView.text = it 은 같은 표현이다.
+             */
             textView.text = it
         })
         return root
